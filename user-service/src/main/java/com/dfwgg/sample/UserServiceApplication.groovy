245663321db597e08910b2948f535a3058aff806
Steve Good <steve@stevegood.org>
2015-02-28 19:13:59 -0600

    Update and rename UserServiceApplication.java to UserServiceApplication.groovy

---
+++ user-service/src/main/java/com/dfwgg/sample/UserServiceApplication.groovy
@@ -0,0 +1,14 @@
+package com.dfwgg.sample
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+@SpringBootApplication
+@EnableJpaRepositories
+class UserServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args)
+    }
+}
