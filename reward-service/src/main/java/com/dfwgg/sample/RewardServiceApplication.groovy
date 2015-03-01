efe56e0f7001c9bd4b9b2d13ec77fb6e39659a63
Steve Good <steve@stevegood.org>
2015-02-28 19:14:45 -0600

    Update and rename RewardServiceApplication.java to RewardServiceApplication.groovy

---
+++ reward-service/src/main/java/com/dfwgg/sample/RewardServiceApplication.groovy
@@ -0,0 +1,12 @@
+package com.dfwgg.sample
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class RewardServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(RewardServiceApplication.class, args)
+    }
+}
